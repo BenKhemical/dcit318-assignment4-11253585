1d306d859e36e53cbfaea01848a59c005b291ceb
BenKhemical <mensahbernard006@gmail.com>
2025-08-07 21:58:08 +0000

    Windows Forms-based Medical Appointment Booking System

--- obj/Debug/net6.0-windows/MedicalApp.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/MedicalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c468bcc5cd6216e865172df87217195ffcbe49")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
